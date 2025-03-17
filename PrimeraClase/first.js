02f4cc630fc76d4325c5f903db41e3ecca995071
NahuelPajon <nahuel.pajon@correo.ucu.edu.uy>
2025-03-17 20:32:10 -0300

    actualizado 1.1

--- PrimeraClase/first.js
+++ PrimeraClase/first.js
@@ -1,4 +1,10 @@
-function main(){
+async function main(){
     console.log("hello world");
+    const url = "https://openlibrary.org/search.json?q=the+lord+of+the+rings";
+    const req = await fetch (url);
+    if (req.ok){
+        const data = await req.json();
+        console.log(data);
+    }
 }
 main();
